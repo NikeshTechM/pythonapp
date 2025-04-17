ef0727e6717f6111ac3d3018a4f46a58a51e8ef0
NikeshTechM <nikesh.sar@techmahindra.com>
2025-04-17 12:40:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Welcome to New Python App- 07Apr2025-9:51am"
+    return "Hello, Welcome to New Python App- 07Apr2025-12:39pm"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
